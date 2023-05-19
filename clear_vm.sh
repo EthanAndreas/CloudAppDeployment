5664523bb885ac0a045148e0d12a7a39b0813033
LosKeeper <thomas.dumond2001@gmail.com>
2023-05-20 00:36:04 +0200

    adding scritp to clear vm

---
+++ clear_vm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Stop all services
+sudo systemctl stop nomad
+sudo systemctl stop consul
+sudo pkill nomad
+sudo pkill consul
+
+
+# Remove all files in /opt/nomad and /opt/consul
+cd /opt/nomad
+sudo rm -rf *
+cd /opt/consul
+sudo rm -rf *
